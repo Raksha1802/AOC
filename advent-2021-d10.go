fddd2c70e419377b8323961c024bc8bbf6c840f9
Raksha Prakasha <rakshamp95@gmail.com>
2022-05-21 21:43:57 +0200

    Iterate over whole input

--- advent-2021-d10.go
+++ advent-2021-d10.go
@@ -1,33 +1,34 @@
 package main
 
-import "fmt"
-
 func main() {
 	opened := []string{}
 	errors := []string{}
-	input := inputs[2]
+	//input := inputs[2]
 
-	for i := 0; i < len(input); i++ {
+	for _, input := range inputs {
+		for i := 0; i < len(input); i++ {
 
-		currentChar := string(input[i])
+			currentChar := string(input[i])
 
-		if _, isOpener := openers[currentChar]; isOpener {
-			opened = append(opened, currentChar)
-			continue
-		} else if _, isCloser := closers[currentChar]; !isCloser {
-			continue
-		}
+			if _, isOpener := openers[currentChar]; isOpener {
+				opened = append(opened, currentChar)
+				continue
+			} else if _, isCloser := closers[currentChar]; !isCloser {
+				continue
+			}
 
-		fmt.Printf("\ncurrentChar: %s", currentChar)
-		fmt.Printf("\nopened[len(opened)-1]: %s", opened[len(opened)-1])
+			//fmt.Printf("\ncurrentChar: %s", currentChar)
+			//fmt.Printf("\nopened[len(opened)-1]: %s", opened[len(opened)-1])
 
-		if opened[len(opened)-1] == closers[currentChar] {
-			opened = opened[:len(opened)-2]
-			continue
-		} else {
-			errors = append(errors, currentChar)
+			if opened[len(opened)-1] == closers[currentChar] {
+				opened = opened[:len(opened)-2]
+				continue
+			} else {
+				errors = append(errors, currentChar)
+			}
 		}
 	}
+
 }
 
 var closers = map[string]string{
