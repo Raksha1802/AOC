9ab796f3ab1a59786f7a5969030c2a270a7d44b5
Raksha Prakasha <rakshamp95@gmail.com>
2022-05-21 22:11:46 +0200

    Map for Errors

--- advent-2021-d10.go
+++ advent-2021-d10.go
@@ -3,42 +3,34 @@ package main
 import "fmt"
 
 func main() {
+
 	opened := []string{}
-	errors := []string{}
-	//input := inputs[0]
+	errors := map[string]int{}
 
 	for _, input := range inputs {
 		for i := 0; i < len(input); i++ {
-
 			currentChar := string(input[i])
-
 			if _, isOpener := openers[currentChar]; isOpener {
 				opened = append(opened, currentChar)
 				continue
 			} else if _, isCloser := closers[currentChar]; !isCloser {
 				continue
 			}
-
 			if opened[len(opened)-1] == closers[currentChar] {
 				opened = opened[:len(opened)-1]
 				continue
 			} else {
-				errors = append(errors, currentChar)
+				errors[currentChar]++
 				break
 			}
 		}
 	}
-
-	fmt.Println()
-	fmt.Println("length", errors)
-
-	var score int
-	for i := range errors {
-		fmt.Println()
-		fmt.Println(errors[i])
-		score += scores[errors[i]]
-		fmt.Printf("\nScore %d", score)
+	var total int = 0
+	for err, times := range errors {
+		fmt.Printf("\n %s: %d", err, times)
+		total += scores[err] * times
 	}
+	fmt.Printf("\ntotal: %d", total)
 }
 
 var closers = map[string]string{
@@ -54,6 +46,7 @@ var openers = map[string]string{
 	"{": "}",
 	"<": ">",
 }
+
 var scores = map[string]int{
 	")": 3,
 	"]": 57,
